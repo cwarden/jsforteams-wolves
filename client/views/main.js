f58b9172a4a4d714aeeca6048c7a8eb930512aaa
Christian G. Warden <cwarden@xerus.org>
2014-07-24 14:13:23 -0700

    Use ampersand-dom to set active class on active nav item

--- client/views/main.js
+++ client/views/main.js
@@ -1,5 +1,6 @@
 var View = require('ampersand-view');
 var ViewSwitcher = require('ampersand-view-switcher');
+var dom = require('ampersand-dom');
 var templates = require('../../build/templates');
 
 module.exports = View.extend({
@@ -18,6 +19,7 @@ module.exports = View.extend({
 	},
 	handlePage: function(pageView) {
 		this.pages.set(pageView);
+		this.setActiveNavItem();
 	},
 	handleLinkClick: function(event) {
 		var aTag = event.target;
@@ -26,5 +28,15 @@ module.exports = View.extend({
 			event.preventDefault();
 			app.router.history.navigate(aTag.pathname, { trigger: true });
 		}
+	},
+	setActiveNavItem: function() {
+		var path = window.location.pathname;
+		this.getAll('[role=nav-items] a').forEach(function(aTag) {
+			if (aTag.pathname === path) {
+				dom.addClass(aTag.parentNode, 'active');
+			} else {
+				dom.removeClass(aTag.parentNode, 'active');
+			}
+		});
 	}
 });
