bb9f175fe2489f03c6d060164ad1b4bad254f0bc
Christian G. Warden <cwarden@xerus.org>
2014-07-24 16:50:22 -0700

    Add support for creating howls
    Add support for creating howls by sending auth token.

--- client/models/howl.js
+++ client/models/howl.js
@@ -2,6 +2,13 @@ var Model = require('ampersand-model');
 var moment = require('moment');
 
 module.exports = Model.extend({
+	ajaxConfig: function() {
+		return {
+			headers: {
+				'Auth-Token': app.me.accessToken
+			}
+		};
+	},
 	props: {
 		id: 'string',
 		content: 'string',
