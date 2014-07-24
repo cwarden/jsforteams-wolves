3e58f1d11a6b46149401bd6b91bbef27eb01b54f
Christian G. Warden <cwarden@xerus.org>
2014-07-24 11:07:25 -0700

    Use getconfig to enable/disable dev mode

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var hapi = require('hapi');
 var moonboots = require('moonboots_hapi');
+var config = require('getconfig');
 
 var server = hapi.createServer(8080, 'localhost');
 
@@ -9,7 +10,7 @@ server.pack.register({
 		appPath: '/{p*}',
 		moonboots: {
 			main: __dirname + '/client/app.js',
-			developmentMode: true,
+			developmentMode: config.isDev,
 			stylesheets: [
 				__dirname + '/public/css/bootstrap.css'
 			]
