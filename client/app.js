76f694771ef5c6f8a1e00928a0ccedf101ea7a01
Christian G. Warden <cwarden@xerus.org>
2014-07-24 10:30:45 -0700

    Use moonboots_hapi to build and serve client app

---
+++ client/app.js
@@ -0,0 +1,7 @@
+window.app = {
+	init: function() {
+		console.log('hello world!');
+	}
+};
+
+window.app.init();
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 var hapi = require('hapi');
+var moonboots = require('moonboots_hapi');
 
 var server = hapi.createServer(8080, 'localhost');
+
+server.pack.register({
+	plugin: moonboots,
+	options: {
+		appPath: '/{p*}',
+		moonboots: {
+			main: __dirname + '/client/app.js',
+			developmentMode: false
+		}
+	}
+}, function() {
+	server.start();
+	console.log('wolves running');
+});
 
 server.start();
