4a8f21f8a64861fb2bae11ce1a62bb6783f56e74
Christian G. Warden <cwarden@xerus.org>
2014-07-25 10:18:42 -0700

    Implement New Howl creation
    Bind to submit event and create a new howl.

--- client/views/new-howl.js
+++ client/views/new-howl.js
@@ -3,5 +3,27 @@ var templates = require('../../build/templates');
 
 module.exports = View.extend({
 	template: templates.includes.newHowl,
-	autoRender: true
+	autoRender: true,
+
+	events: {
+		'submit [role=new-howl]': 'createNewHowl'
+	},
+
+	createNewHowl: function(event) {
+		event.preventDefault();
+		var self = this;
+		app.howls.create({
+			content: this.get('[name=content]').value,
+			createdAt: new Date()
+		}, {
+			wait: true,
+			success: function() {
+				self.get('[name=content]').value = '';
+			},
+			error: function() {
+				alert('FAILED!');
+			}
+		});
+	}
+
 });
